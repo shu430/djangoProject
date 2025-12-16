f4a5c22cb5daab1053f545f0298457e7ae16bfb6
shu430 <syuu04301@gmail.com>
2025-12-17 00:49:05 +0900

    [add]CRUD操作を追加しタスク操作可能にした

---
+++ todoproject/todoapp/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from todoapp.models import Task
+
+# Create your views here.
+class TaskList(ListView):
+    model = Task
+    context_object_name =  "tasks"
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__" #['uesr', 'title',...]
+    success_url = reverse_lazy("tasks")
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__" #['uesr', 'title',...]
+    success_url = reverse_lazy("tasks")
+
+class TaskDelete(DeleteView):
+    model = Task
+    fields = "__all__" #['uesr', 'title',...]
+    success_url = reverse_lazy("tasks")
